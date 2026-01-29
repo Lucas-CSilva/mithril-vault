0aaac34f2e9afe2d72c6e1e8ad7517bc1225784d
lucas correia <lucas.correia010103@gmail.com>
2026-01-28 22:09:53 -0300

    fix(web): update NEXT_PUBLIC_API_URL to include the correct API path

--- web/next.config.ts
+++ web/next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
   },
   env: {
     NEXT_PUBLIC_API_URL:
-      process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
+      process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api/mithril-vault",
   },
 };
 
