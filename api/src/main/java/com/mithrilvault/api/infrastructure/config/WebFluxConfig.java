9248a3067e751bcf3896818b759e6bfc6a7642f3
lucas correia <lucas.correia010103@gmail.com>
2026-01-28 21:52:20 -0300

    chore(api): remove unnecessary @EnableWebFlux annotation from WebFluxConfig

--- api/src/main/java/com/mithrilvault/api/infrastructure/config/WebFluxConfig.java
+++ api/src/main/java/com/mithrilvault/api/infrastructure/config/WebFluxConfig.java
@@ -2,11 +2,9 @@ package com.mithrilvault.api.infrastructure.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.ServerCodecConfigurer;
-import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-@EnableWebFlux
 public class WebFluxConfig implements WebFluxConfigurer {
 
   @Override
